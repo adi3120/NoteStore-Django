04017a300d63a269f68bc397f9e46a701ad319e1
adi3120 <aditya.yadav.cs.20@ggits.net>
2023-03-05 20:33:24 +0530

    Vercel Setting

--- demo1/settings.py
+++ demo1/settings.py
@@ -74,14 +74,14 @@ WSGI_APPLICATION = 'demo1.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        # 'ENGINE': 'django.db.backends.mysql',
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         # 'ENGINE': 'django.db.backends.mysql',
 
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
@@ -123,4 +123,7 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
+import os
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
